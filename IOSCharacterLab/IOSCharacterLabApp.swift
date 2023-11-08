8f78659f332a507d1f626988e7c51e20f8cfcecd
Michael Vacca <michaelvacca07@gmail.com>
2023-11-08 17:04:21 -0500

    Initial Commit

---
+++ IOSCharacterLab/IOSCharacterLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSCharacterLabApp.swift
+//  IOSCharacterLab
+//
+//  Created by macuser on 2023-11-08.
+//
+
+import SwiftUI
+
+@main
+struct IOSCharacterLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
